834d16621d25e7b595fa410f80cbca2aaa25b974
mahmoud2391997 <114721167+mahmoud2391997@users.noreply.github.com>
2023-09-05 15:40:51 +0300

    Initial

---
+++ JS/tasks/week1.js
@@ -0,0 +1,24 @@
+console.log("%cElzero %cWeb %cSchool","font-size:40px; color:red;","font-size:40px; color:green; font-weight:bold;","font-size:40px; color:white; background-color:blue;")
+console.group("Group 1")
+console.log("one")
+console.log("two")
+console.group("Group 1.1")
+console.log("one")
+console.log("two")
+console.group("Group 1.1.1")
+console.log("one")
+console.log("two")
+console.groupEnd()
+console.groupEnd()
+console.groupEnd()
+console.group("group two")
+console.log("one")
+console.log("two")
+console.groupEnd()
+console.table(["0","1","2","3","4"])
+// console.log("Iam In Console");
+// document.write("Iam In Page");
+/*
+console.log("Iam In Console");
+document.write("Iam In Page");
+*/
---
+++ JS/week1.js
@@ -0,0 +1,12 @@
+
+// Single line comment
+/*
+1
+2
+3
+*/
+console.log('log');
+console.log('Error');
+console.log('hello from %cfile',"color:red; font-size:40px");
+console.table(['Error','ahmed',]);
+// ES6
